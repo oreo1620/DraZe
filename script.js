944402f59516a8d606bb71b9901b0c98af28da0b
oreo1620 <yasminpilapil1620@gmail.com>
2024-12-10 23:05:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,123 @@
+const kdramaItems = [
+    { name: "A Business Proposal", img: "img/K/ABP.jpg", link: "https://kisskh.co/Drama/A-Business-Proposal?id=4608&q=true" },
+    { name: "Behind Your Touch", img: "img/K/BYT.jpg", link: "https://kisskh.co/Drama/Behind-Your-Touch?id=7799&q=true" },
+    { name: "All of Us are Dead", img: "img/K/AOUAD.jpg", link: "https://kisskh.co/Drama/All-of-Us-Are-Dead?id=4531&q=true" },
+    { name: "Celebrity", img: "img/K/CELEB.jpg", link: "https://kisskh.co/Drama/Celebrity?id=7510&q=true" },
+    { name: "Crash Landing On You", img: "img/K/CLOY.jpg", link: "https://kisskh.co/Drama/Crash-Landing-on-You?id=97&q=true" },
+    { name: "Destined With You", img: "img/K/DWY.jpg", link: "https://kisskh.co/Drama/Destined-With-You?id=7804&q=true" },
+    { name: "From Now On, Showtime", img: "img/K/FNST.jpg", link: "https://kisskh.co/Drama/From-Now-On--Showtime-?id=4816&q=true" },
+    { name: "Flex X Cop", img: "img/K/FXC.jpg", link: "https://kisskh.co/Drama/Flex-X-Cop?id=8534&q=true" },
+    { name: "Hotel Del Luna", img: "img/K/HDL.jpg", link: "https://kisskh.co/Drama/Hotel-del-Luna?id=39&q=true" },
+    { name: "I'ts Okay to Not Be Okay", img: "img/K/IOTNBO.jpg", link: "https://kisskh.co/Drama/It-s-Okay-to-Not-Be-Okay?id=30&q=true" },
+    { name: "King The Land", img: "img/K/KTL.jpg", link: "https://kisskh.co/Drama/King-the-Land?id=7460&q=true" },
+    { name: "Love All Play", img: "img/K/LAP.jpg", link: "https://kisskh.co/Drama/Love-All-Play---Going-to-You-at-a-Speed-of-493-km?id=4815&q=true" },
+    { name: "Love Next Door", img: "img/K/LND.jpg", link: "https://kisskh.co/Drama/Love-Next-Door?id=9301&q=true" },
+    { name: "Lovely Runner", img: "img/K/LR.jpg", link: "https://kisskh.co/Drama/Lovely-Runner?id=8792&q=true" },
+    { name: "Moving", img: "img/K/M.jpg", link: "https://kisskh.co/Drama/Moving?id=7793&q=true" },
+    { name: "My Demon", img: "img/K/MD.jpg", link: "https://kisskh.co/Drama/My-Demon?id=8268&q=true" },
+    { name: "Queen Of Tears", img: "img/K/QOT.jpg", link: "https://kisskh.co/Drama/Queen-of-Tears?id=8714&q=true" },
+    { name: "Strong Girl Nam Soon", img: "img/K/SGNS.jpg", link: "https://kisskh.co/Drama/Strong-Girl-Namsoon?id=7940&q=true" },
+    { name: "Strong Woman Do Bong Soon", img: "img/K/SWDBS.jpg", link: "https://kisskh.co/Drama/Strong-Woman-Do-Bong-Soon?id=44&q=true" },
+    { name: "See You In My 19th Life", img: "img/K/SYIm19L.jpg", link: "https://kisskh.co/Drama/See-You-in-My-19th-Life?id=7461&q=true" },
+    { name: "True Beauty", img: "img/K/TB.jpg", link: "https://kisskh.co/Drama/True-Beauty?id=975&q=true" },
+    { name: "The Glory", img: "img/K/TG.jpg", link: "https://kisskh.co/Drama/The-Glory-Season-1?id=6514&q=true" },
+    { name: "Vincenzo", img: "img/K/V.jpg", link: "https://kisskh.co/Drama/Vincenzo--2021-?id=1219&q=true" },
+    { name: "Weightlifting Fairy Kim Bok Joo", img: "img/K/WLFBJ.jpg", link: "https://kisskh.co/Drama/Weightlifting-Fairy-Kim-Bok-Joo?id=120&q=true" },
+];
+
+const cdramaItems = [
+    { name: "Amidst a Snowstorm of Love", img: "img/C/AASTOL.jpg", link: "https://kisskh.co/Drama/Amidst-a-Snowstorm-of-Love?id=8648&q=true" },
+    { name: "As Beautiful As You", img: "img/C/ABAY.jpg", link: "https://kisskh.co/Drama/As-Beautiful-As-You?id=7943&q=true" },
+    { name: "Everyone Loves Me", img: "img/C/ELM.jpg", link: "https://kisskh.co/Drama/Everyone-Loves-Me?id=8619&q=true" },
+    { name: "Flourish In Time", img: "img/C/FIT.jpg", link: "https://kisskh.co/Drama/Flourish-in-Time?id=2551&q=true" },
+    { name: "Fireworks of My Heart", img: "img/C/FOMH.jpg", link: "https://kisskh.co/Drama/Fireworks-of-My-Heart?id=7628&q=true" },
+    { name: "Intense Love", img: "img/C/IL.jpg", link: "https://kisskh.co/Drama/Intense-Love?id=2522&q=true" },
+    { name: "My Boss", img: "img/C/MB.jpg", link: "https://kisskh.co/Drama/My-Boss?id=8273&q=true" },
+    { name: "Only For Love", img: "img/C/OFL.jpg", link: "https://kisskh.co/Drama/Only-for-Love?id=7949&q=true" },
+    { name: "Our Secret", img: "img/C/OS.jpg", link: "https://kisskh.co/Drama/Our-Secret?id=3368&q=true" },
+    { name: "Time and Him are Just Right", img: "img/C/TAHAJR.jpg", link: "https://kisskh.co/Drama/Meeting-You-Is-Luckiest-Thing-to-Me---Time-and-Him-are-Just-Right?id=6757&q=true" },
+    { name: "Unforgettable Love", img: "img/C/UL.jpg", link: "https://kisskh.co/Drama/Unforgettable-Love?id=2897&q=true" },
+    { name: "When I Fly Towards You", img: "img/C/WIFTY.jpg", link: "https://kisskh.co/Drama/When-I-Fly-Towards-You?id=7532&q=true" },
+    { name: "You Are My Glory", img: "img/C/YAMG.jpg", link: "https://kisskh.co/Drama/You-Are-My-Glory--2021-?id=2621&q=true" },
+    { name: "You Are My Hero", img: "img/C/YAMH.jpg", link: "https://kisskh.co/Drama/You-Are-My-Hero?id=1408&q=true" },
+    { name: "You Are My Secret", img: "img/C/YAMS.jpg", link: "https://kisskh.co/Drama/You-Are-My-Secret?id=9224&q=true" },
+    { name: "Cute Programmer", img: "img/C/CP.jpg", link: "https://kisskh.co/Drama/Cute-Programmer?id=3753&q=true" },
+    { name: "The Oath of Love", img: "img/C/TOOL.jpg", link: "https://kisskh.co/Drama/The-Oath-of-Love--2022-?id=3330" },
+    { name: "Love Me, Love My Voice", img: "img/C/LMLMV.jpg", link: "https://kisskh.co/Drama/Missing-You---Love-Me--Love-My-Voice?id=7983&q=true" },
+];
+
+const otherItems = [
+    { name: "Alice in Borderland", img: "img/O/AIBL.jpg", link: "https://kisskh.co/Drama/Alice-in-Borderland?id=1982&q=true" },
+    { name: "Anyone But You", img: "img/O/ABY.jpg", link: "https://kisskh.co/Drama/Anyone-But-You?id=8673&q=true" },
+    { name: "And Yet, You Are So Sweet", img: "img/O/AYYASS.jpg", link: "https://kisskh.co/Drama/And-Yet--You-Are-So-Sweet?id=8707&q=true" },
+    { name: "Cursed In Love", img: "img/O/CIL.jpg", link: "https://kisskh.co/Drama/Watashitachi-wa-Douka-Shiteiru---cursed-in-love?id=3369&q=true" },
+    { name: "Damsel", img: "img/O/DSL.jpg", link: "https://kisskh.co/Drama/Damsel?id=8761&q=true" },
+    { name: "Eye Love You", img: "img/O/ELY.jpg", link: "https://kisskh.co/Drama/Eye-Love-You?id=8641&q=true" },
+    { name: "Incurable Case of Love", img: "img/O/ICOL.jpg", link: "https://kisskh.co/Drama/An-Incurable-Case-of-Love?id=2952&q=true" },
+    { name: "Mean Girls", img: "img/O/MG.jpg", link: "https://cinego.tv/watch-movie/watch-mean-girls-online-18854/1064354" },
+    { name: "To All The Boys I've Loved Before", img: "img/O/TATBILB.jpg", link: "https://cinego.tv/watch-movie/watch-to-all-the-boys-ive-loved-before-online-19779/933936" },
+    { name: "The Parent Trap", img: "img/O/TPT.jpg", link: "https://cinego.tv/watch-movie/watch-the-parent-trap-online-17583/935251" },
+    { name: "White Chicks", img: "img/O/WC.jpg", link: "https://cinego.tv/watch-movie/watch-white-chicks-online-18061/934974" },
+    { name: "Purple Hearts", img: "img/O/PH.jpg", link: "https://cinego.tv/watch-movie/watch-purple-hearts-online-83245/1281546" },
+];
+
+// Function to display items on the page
+function displayItems(itemsArray) {
+    const contentDiv = document.getElementById("content");
+    contentDiv.innerHTML = ""; // Clear existing items
+
+    itemsArray.forEach(item => {
+        const galleryDiv = document.createElement("div");
+        galleryDiv.classList.add("gallery");
+        galleryDiv.innerHTML = `
+            <a href="${item.link}">
+                <img src="${item.img}" alt="${item.name}">
+            </a>
+            <div class="description">${item.name}</div>
+        `;
+        contentDiv.appendChild(galleryDiv);
+    });
+}
+
+// Detect which page we are on and load the correct items
+if (window.location.pathname.includes("main.html")) {
+    displayItems([...kdramaItems, ...cdramaItems, ...otherItems]);
+} else if (window.location.pathname.includes("kDrama.html")) {
+    displayItems(kdramaItems);
+} else if (window.location.pathname.includes("cDrama.html")) {
+    displayItems(cdramaItems);
+} else if (window.location.pathname.includes("others.html")) {
+    displayItems(otherItems);
+}
+
+function searchItems() {
+    const input = document.getElementById('searchInput');
+    const filter = input.value.toLowerCase();
+    const galleryItems = document.querySelectorAll('.gallery');
+    const searchResultsHeader = document.getElementById('searchResults');
+
+    // Reset search results header
+    if (filter) {
+        searchResultsHeader.textContent = `Results for: "${filter}"`;
+    } else {
+        searchResultsHeader.textContent = ''; // Clear the header if input is empty
+    }
+
+    let resultsFound = false; // Track if any results are found
+
+    galleryItems.forEach(item => {
+        const title = item.querySelector('.description').textContent;
+        if (title.toLowerCase().includes(filter)) {
+            item.style.display = ""; // Show item
+            resultsFound = true; // Found at least one result
+        } else {
+            item.style.display = "none"; // Hide item
+        }
+    });
+
+    // If no results were found, indicate that
+    if (!resultsFound) {
+        searchResultsHeader.textContent = `No results found for: "${filter}"`;
+    }
+}
+
